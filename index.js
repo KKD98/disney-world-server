34faa6b58c11a01d56844a6448ff38e9a4822109
KKD98 <KajolKona98@gmail.com>
2023-05-19 02:31:44 +0600

    alltoys route implemented from mongodb done

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toysServer').collection('toys');
+
+    app.get('/alltoys' , async(req , res) => {
+      const toys = toysCollection.find();
+      const result = await toys.toArray();
+      res.send(result);
+    })
+
+    app.get('/toysbycategory/:category' , async(req , res) => {
+      const toys = toysCollection.find({category: req.params.category});
+      const result = await toys.toArray();
+      res.send(result);
+    } )
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
